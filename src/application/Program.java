8ebbc2e57f353eb10869fa83ec32a2eedd4befbf
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2026-01-02 08:17:58 -0300

    Vesão 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,37 +1,29 @@
 package application;
 
+import util.Calculator;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    //valor final constante (não muda mais) -> padrão tudo maiusculo
-    public static double PI = 3.14159;
-
     public static final void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
 
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = circumference(radius);
+        double c = calc.circumference(radius);
 
-        double v = volume(radius);
+        double v = calc.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI Value: %.2f%n", PI);
+        System.out.printf("PI Value: %.2f%n", calc.PI);
 
 
         sc.close();
-    }
-
-    public static double circumference(double radius) {
-        return 2.0 * PI * radius;
-    }
-
-    public static double volume(double radius) {
-        return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
